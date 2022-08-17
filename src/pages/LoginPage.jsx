a4bd62b414a336004e77ede51c6de66cf46595ae
Yevhen Podoliaka <zanp85740@gmail.com>
2022-08-17 21:09:19 +0300

    delete rtkQuery

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,72 @@
+import { useState } from 'react';
+
+
+const styles = {
+  form: {
+    width: 320,
+  },
+  label: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: 15,
+  },
+};
+
 export default function LoginPage() {
-  return <h2>loginPage</h2>;
+
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  
+    setEmail('');
+    setPassword('');
+  };
+  return (
+    <div>
+      <h2>loginPage</h2>
+      <form onSubmit={handleSubmit} style={styles.form} autoComplete="off">
+        <label style={styles.label}>
+          email
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label style={styles.label}>
+          password
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+
 }
+
+
+
+
+
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,3 +1,78 @@
+import { useState } from "react";
+
+
+const styles = {
+  form: {
+    width: 320,
+  },
+  label: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: 15,
+  },
+
+};
 export default function RegisterPage() {
-  return <h2>RegisterPage</h2>;
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+ 
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'name':
+        return setName(value);
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <h2>RegisterPage</h2>
+
+      <form onSubmit={handleSubmit} style={styles.form} autoComplete="off">
+        <label style={styles.label}>
+          name
+          <input type="text" name="name" value={name} onChange={handleChange} />
+        </label>
+
+        <label style={styles.label}>
+          email
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label style={styles.label}>
+          password
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">registred</button>
+      </form>
+    </div>
+  );
 }
